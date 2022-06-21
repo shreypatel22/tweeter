90b8d40df8fd20e7d61f7072f8680210aa512a39
Shrey Patel <shreypatel11@hotmail.com>
2022-06-21 17:33:47 -0400

    Update counter for form and simple CSS

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready( () => {
+  $('textarea').on('input', function() {
+    // This selects the input text and then finds its length
+    let inputLength = $(this).val().length
+
+    // This selects the parent of the textarea (form) then finds the output tag with find()
+      // ^that selects the node, to get the text you do .text()      
+    let counterValue = $(this).parent().find('output').text(140 - inputLength);
+    
+    // if (Number(counterValue.text()) < 0) {
+    //   console.log(counterValue.text())
+    //   counterValue.css('color', 'red')
+    // } else {
+    //   counterValue.css('color', '')
+    // }
+
+    if (Number(counterValue.text()) < 0) {
+      console.log(counterValue.text())
+      counterValue.addClass('counter-red')
+    } else {
+      counterValue.removeClass('counter-red')
+    }
+  })
+});  
