4c1ce2b78e0fc26cc21753bbfdc93b530e9f0b12
Shrey Patel <shreypatel11@hotmail.com>
2022-06-22 14:23:55 -0400

    Add ajax request to sumbit tweet

---
+++ public/scripts/submit-tweet.js
@@ -0,0 +1,20 @@
+const $form = $('form');
+const $tweetData = $('#tweet-text');
+
+$form.on('submit', (event) => {
+  // you need the parameter event in the callback so you can use preventDefault which stops the page from redirecting
+  event.preventDefault();
+
+  // const $tweetText = $tweetData.val();
+  // console.log($tweetText);
+
+  $.ajax({
+    type: "POST",
+    url: '/tweets',
+    // Use $form.serialize here instead of $tweetData since if you have more than one text area in the form you can get all with form
+    // with $tweetData you could only get the one you selected and that defeats the whole serization process
+    data: $form.serialize(),
+    success: () => {console.log($form.serialize())}    
+  });
+
+})
