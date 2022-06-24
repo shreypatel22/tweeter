99a5ff5fece10f42c21cd6d2456fa0bbbbb61ea6
Shrey Patel <shreypatel11@hotmail.com>
2022-06-24 17:49:39 -0400

    More CSS updating (colors and fixing some bugs)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -42,14 +42,14 @@ const createTweetElement = (tweetData) => {
   <article>
     <header>
       <div>
-      <img src=${tweetData.user.avatars} alt="profile icon">
-        <p class="person-name">${tweetData.user.name}</p>
+        <img src=${tweetData.user.avatars} alt="profile icon">
+          <p class="person-name">${tweetData.user.name}</p>
       </div>
-      <div>${tweetData.user.handle}</div>
+      <div class="tweeter-handle">${tweetData.user.handle}</div>
     </header>
     <p>${convertToSafe(tweetData.content.text)}</p>
-    <footer class="tweet-footer">
-      <div >${timeago.format(tweetData.created_at)}</div>
+    <footer>
+      <div class="tweet-footer-left">${timeago.format(tweetData.created_at)}</div>
       <div class="tweet-footer-right">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -68,17 +68,17 @@ $form.on('submit', (event) => {
   
   const $tweetData = $('#tweet-text');
   const $tweetText = $tweetData.val();
-  // const $tweetError = $('.error')
+
   const $tweetErrorText = $tweetError.find('#tweet-error-text')
-  // console.log($tweetText);
+
   if ($tweetText.length > 140) {
-    // $tweetError.show();
+
     $tweetError.slideUp();
     $tweetError.slideDown();
 
     $tweetErrorText.text('Please keep to 140 characters or less!')
   } else if (!$tweetText) {
-    // $tweetError.show();
+
     $tweetError.slideUp();
     $tweetError.slideDown();
     $tweetErrorText.text('Please enter something in order to tweet!')
@@ -87,8 +87,7 @@ $form.on('submit', (event) => {
     $.ajax({
       type: "POST",
       url: '/tweets',
-      // Use $form.serialize here instead of $tweetData since if you have more than one text area in the form you can get all with form
-      // with $tweetData you could only get the one you selected and that defeats the whole serization process
+   
       data: $form.serialize(),      
       success: () => {
         // Load loads -> now adds the new tweet before loading
